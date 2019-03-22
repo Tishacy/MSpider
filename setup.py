6823c4ed966f8b0310297ff4976d88f92b6a668e
Tishacy <tishacy@gmail.com>
2019-03-22 15:44:30 +0800

    fix bugs of packages dependencies error when installing using pip on Windows

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="mspider",
-    version="0.2.1",
+    version="0.2.2",
     author="Tishacy",
     author_email="",
     description="Make your spider multi-threaded.",
@@ -13,9 +13,9 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/Tishacy/MSpider",
     packages=setuptools.find_packages(),
-    install_requires=["numpy==1.14.4","requests==2.18.4",
-                      "pandas==0.24.1","tqdm==4.30.0",
-                      "wget==3.2","beautifulsoup4==4.7.1"],
+    install_requires=["numpy>=1.14.4","requests>=2.18.4",
+                      "pandas>=0.24.1","tqdm>=4.30.0",
+                      "wget>=3.2","beautifulsoup4>=4.7.1"],
     entry_points={
         'console_scripts': [
             'genspider=mspider.genspider:genspider'
